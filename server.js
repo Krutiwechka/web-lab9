b28a76e9184f600289e51151c494b212ddd66535
Timojj <krutiwechka@gmail.com>
2026-05-17 16:46:13 +0300

    BREAKING CHANGE: v2

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const PORT = process.env.PORT || 3000;
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    },
+    cors: { origin: "*", methods: ["GET", "POST"] },
     transports: ['websocket']
 });
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/chat', (req, res) => {
-    const username = req.query.username || 'Аноним';
-    res.render('chat', { username });
-});
-
 io.on('connection', (socket) => {
-    
+    let currentUsername = 'Аноним';
+
+    socket.on('user joined', (username) => {
+        currentUsername = username;
+        io.emit('system notification', `${currentUsername} присоединился к чату`);
+    });
+
     socket.on('chat message', (data) => {
         io.emit('chat message', {
             id: Date.now() + Math.random().toString(36).substr(2, 9),
@@ -36,8 +35,12 @@ io.on('connection', (socket) => {
         io.emit('message reaction', data);
     });
 
+    socket.on('disconnect', () => {
+        io.emit('system notification', `${currentUsername} покинул чат`);
+    });
 });
 
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
